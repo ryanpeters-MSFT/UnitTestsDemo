4af7257fa0659a9bb275a02fc10deec0599172b3
Ryan Peters <ryanpeters@microsoft.com>
2022-11-12 22:53:22 -0500

    first commit

---
+++ MyTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+public class MyTests
+{
+    [Fact]
+    public void TestSomething()
+    {
+        Assert.True(5 == 5);
+    }
+
+    [Fact]
+    public void SomethingTrue()
+    {
+        var calc = (int first, int second) => first + second;
+
+        Assert.Equal(5, calc(2, 3));
+    }
+
+    [Fact]
+    public void BadCalculator()
+    {
+        var calc = (int first, int second) => first + second + 9999;
+
+        Assert.Equal(5, calc(2, 3));
+    }
+}
